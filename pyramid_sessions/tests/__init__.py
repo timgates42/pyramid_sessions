8d6973c5068c0b544c2d9210185594ebec06c701
Julian Paul Glass <joules@magemx.com>
2015-01-09 08:41:40 +0000

    add change location of tests.

---
+++ pyramid_sessions/tests/__init__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from pyramid.config import Configurator
+from pyramid.response import Response
+from pyramid.session import SignedCookieSessionFactory
+
+TEST_SESSIONS = ('session1', 'session2', 'session3', 'session4')
+
+def test_app(global_config, **settings):
+    """Test app for pyramid_sessions testing.
+    """
+    config = Configurator(settings=settings)
+    config.include('pyramid_sessions')
+    config.add_route('sessions', '/{session}')
+    config.add_route('set_sessions', '/set/{session}')
+    config.add_route('get_sessions', '/get/{session}')
+    config.add_view(test_session, route_name='sessions')
+    config.add_view(test_set_session, route_name='set_sessions')
+    config.add_view(test_get_session, route_name='get_sessions')
+    for i in TEST_SESSIONS:
+        config.add_session_factory(i,
+            SignedCookieSessionFactory('secret', cookie_name=i))
+    return config.make_wsgi_app()
+
+def test_session(request):
+    session_id = request.matchdict['session']
+    session = request.sessions.get(session_id)
+    return Response()
+
+def test_set_session(request):
+    session_id = request.matchdict['session']
+    session = request.sessions.get(session_id)
+    session['state'] = session_id
+    return Response()
+
+def test_get_session(request):
+    session_id = request.matchdict['session']
+    session = request.sessions.get(session_id)
+    return Response(session['state'])
+
+# vim:set ts=4 sts=4 sw=4 et tw=79: 
---
+++ pyramid_sessions/tests/test_pyramid_sessions.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+"""
+test_pyramid_sessions
+----------------------------------
+
+Tests for `pyramid_sessions` module.
+"""
+import unittest
+
+from pyramid import testing
+from pyramid.request import Request
+from pyramid.response import Response
+from pyramid.session import SignedCookieSessionFactory
+from pyramid.threadlocal import get_current_request
+
+class Test_config(unittest.TestCase):
+    def _makeOne(self, request, **kw):
+        from pyramid.session import SignedCookieSessionFactory
+        kw.setdefault('secret', 'secret')
+        return SignedCookieSessionFactory(**kw)(request)
+
+    def setUp(self):
+        self.request = testing.DummyRequest()
+        #self.request = Request({})
+        self.config = testing.setUp(request=self.request)
+
+    def test_includeme(self):
+        self.config.include('pyramid_sessions')
+
+    def test_add_session_factory(self):
+        self.test_includeme()
+        session1 = 'session1'
+        session2 = 'session2'
+        sec = 'secret'
+        self.config.add_session_factory('session1', 
+                SignedCookieSessionFactory(sec, cookie_name='session1'))
+        self.config.add_session_factory('session2',
+                SignedCookieSessionFactory(sec, cookie_name='session2'))
+
+    def tearDown(self):
+        testing.tearDown()
+
+class Test_sessions(unittest.TestCase):
+
+    def setUp(self):
+        from webtest import TestApp
+        from pyramid_sessions.tests import test_app
+        self.test_app = TestApp(test_app({}))
+
+    def test_get_session(self):
+        from pyramid_sessions.tests import TEST_SESSIONS
+        for i in TEST_SESSIONS:
+            try:
+                self.test_app.get('/'+i, status=200)
+            except Exception as e:
+                raise self.failureException(e)
+
+    def test_set_session_value(self):
+        from pyramid_sessions.tests import TEST_SESSIONS
+        for id in TEST_SESSIONS:
+            try:
+                self.test_app.get('/set/'+id, status=200)
+            except Exception as e:
+                raise self.failureException(e)
+
+    def test_get_session_value(self):
+        self.test_set_session_value()
+        from pyramid_sessions.tests import TEST_SESSIONS
+        for id in TEST_SESSIONS:
+            try:
+                res = self.test_app.get('/get/'+id, status=200)
+                self.assertEqual(res.body, id.encode())
+            except Exception as e:
+                raise self.failureException(e)
+        
+        
+        
+# vim:set ts=4 sts=4 sw=4 et tw=79:
